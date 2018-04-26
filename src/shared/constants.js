c42c0e5370b373909e9e5fe629fefb17f15c5e3c
Jameson Knoll <j.knoll621@gmail.com>
2018-04-26 23:47:46 +0800

    initial commit

---
+++ src/shared/constants.js
@@ -0,0 +1,9 @@
+export const LIGHT_BLUE = '#0097CF';
+export const DARK_BLUE = '#0487B6';
+export const WHITE = '#FFFFFF';
+export const BODY_BACKGROUND = '#EDF3F3';
+export const LIGHT_GREY = '#C8D4DB';
+export const MEDIUM_GREY = '#9FAD85';
+export const DARK_GREY = '#253439';
+export const HIGHLIGHT_RED = '#FF5858';
+export const HIGHLIGHT_GREEN = '#75D392';
