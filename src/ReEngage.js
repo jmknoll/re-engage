d90a7db08868dc9fd3d8c6c13ef00c51fb4d64f7
Jameson Knoll <j.knoll621@gmail.com>
2018-04-26 23:57:40 +0800

    basic app setup

---
+++ src/ReEngage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import configureStore from './store';
+
+import { Navigation } from 'react-native-navigation';
+import { registerScreens } from './screens';
+
+import { AsyncStorage } from 'react-native';
+
+const store = configureStore();
+
+registerScreens(store, provider);
+
+const navigatorStyle = {
+  statusBarColor: '#831d19',
+  navigationBarColor: '#339999',
+  navBarBackgroundColor: '#339999',
+  navBarTextColor: '#ffffff',
+  navBarButtonColor: '#ffffff',
+  statusBarTextColorScheme: 'light',
+  navBarHidden: false,
+  tabBarButtonColor: 'red',
+  tabBarSelectedButtonColor: 'red',
+  tabBarBackgroundColor: 'red'
+};
+
+const landingNavigatorStyle = {
+  navBarHidden: true,
+  statusBarColor: '#831d19',
+}
+
+async function getCache (item) {
+  try {
+    const data = await AsyncStorage.getItem(item);
+    if (data !== null){
+      return data
+    }
+  } catch (error) {
+  console.log(`error retrieving ${item} from AsyncStorage`)
+  }
+}
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'reEngage.LandingScreen',
+    backButtonHidden: true,
+    navigatorStyle: landingNavigatorStyle
+  }
+})
--- src/screens.js
+++ src/screens.js
@@ -2,4 +2,6 @@ import { Navigation } from 'react-native-navigation';
 
 import LandingScreen from './app/containers/Landing';
 
-export 
+export function registerScreens(store, provider) {
+  Navigation.registerComponent('reEngage.LandingScreen', () => LandingScreen, store, provider);
+}
